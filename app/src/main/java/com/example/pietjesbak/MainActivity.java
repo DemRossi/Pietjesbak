7306ffe2b8b097ea3e6528961ceff16a1fb9919b
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-12-10 16:50:10 +0100

    added images of dices and the roll logic

--- app/src/main/java/com/example/pietjesbak/MainActivity.java
+++ app/src/main/java/com/example/pietjesbak/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView ma_DisplayPlayer1;
     private TextView ma_DisplayPlayer2;
+    private Button ma_RollDicesBtn;
+//    private ImageView ma_Dice1; //, ma_Dice2, ma_Dice3
+    public static final Random RANDOM = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,11 @@ public class MainActivity extends AppCompatActivity {
         ma_DisplayPlayer1 = (TextView) findViewById(R.id.ma_textViewPlayer1);
         ma_DisplayPlayer2 = (TextView) findViewById(R.id.ma_textViewPlayer2);
 
-//        Intent intentThatStartedMainActivity = getIntent();
+//        ma_Dice1 = (ImageView) findViewById(R.id.ma_Dice1);
+//        ma_Dice2 = (TextView) findViewById(R.id.ma_Dice2);
+//        ma_Dice3 = (TextView) findViewById(R.id.ma_Dice3);
+
+        ma_RollDicesBtn = findViewById(R.id.ma_rollBtn);
 
         Bundle playerNames = getIntent().getExtras();
 
@@ -31,5 +43,30 @@ public class MainActivity extends AppCompatActivity {
             ma_DisplayPlayer1.setText(strPlayer1);
             ma_DisplayPlayer2.setText(strPlayer2);
         }
+
+        ma_RollDicesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                int RanDiceVal1 = randomDiceValue();
+//                int RanDiceVal2 = randomDiceValue();
+//                int RanDiceVal3 = randomDiceValue();
+
+//                int res1 = getResources().getIdentifier("Dice_" + RanDiceVal1 ,
+//                        "drawable", "com.example.pietjesbak");
+//                int res2 = getResources().getIdentifier("Dice_" + RanDiceVal2 ,
+//                        "drawable", "com.example.pietjesbak");
+//                int res3 = getResources().getIdentifier("Dice_" + RanDiceVal3 ,
+//                        "drawable", "com.example.pietjesbak");
+
+
+//                ma_Dice1.setText(String.valueOf(RanDiceVal1));
+//                ma_Dice2.setText(String.valueOf(RanDiceVal2));
+//                ma_Dice3.setText(String.valueOf(RanDiceVal3));
+            }
+        });
+    }
+
+    public static int randomDiceValue(){
+        return RANDOM.nextInt(6) +1;
     }
 }
